e180cdd9db39dfe90000226dfb5a42f5929cf654
Thomas Bruederli <thomas@roundcube.net>
2014-05-15 09:01:35 +0200

    Add option to customize the page favion

--- README.md
+++ README.md
@@ -18,6 +18,9 @@ $config['custom_watermark_image'] = './skins/custom_watermark.png';
 // define a custom URI to be displayed instead of the empty watermark page
 $config['custom_watermark_uri'] = '';
 
+// define a custom favicon image (emtpy string to remove it)
+$config['custom_favicon'] = './skins/custom_favicon.ico';
+
 // defines a custom CSS file which is added to every page
 $config['custom_stylesheet'] = './skins/custom_stylez.css';
 ```
--- customizr.php
+++ customizr.php
@@ -14,6 +14,9 @@
  *   // define a custom URI to be displayed instead of the empty watermark page
  *   $config['custom_watermark_uri'] = '';
  *
+ *   // define a custom favicon image (emtpy string to remove it)
+ *   $config['custom_favicon'] = './skins/custom_favicon.ico';
+ *
  *   // defines a custom CSS file which is added to every page
  *   $config['custom_stylesheet'] = './skins/custom_stylez.css';
  *
@@ -24,10 +27,11 @@
 class customizr extends rcube_plugin
 {
     public $noajax = true;
-    public $task = '?(?!login|logout).*';
+    public $task = '?(?!logout).*';
 
     private $rcmail;
     private $custom_css;
+    private $custom_favicon;
     private $watermark_uri;
     private $watermark_image;
 
@@ -38,10 +42,11 @@ class customizr extends rcube_plugin
     {
         $this->rcmail = rcube::get_instance();
         $this->custom_css = $this->rcmail->config->get('custom_stylesheet');
+        $this->custom_favicon = $this->rcmail->config->get('custom_favicon', null);
         $this->watermark_uri = $this->rcmail->config->get('custom_watermark_uri');
         $this->watermark_image = $this->rcmail->config->get('custom_watermark_image');
 
-        if (!empty($this->custom_css) || !empty($this->watermark_uri) || !empty($this->watermark_image)) {
+        if (!empty($this->custom_css) || !is_null($this->custom_favicon) || !empty($this->watermark_uri) || !empty($this->watermark_image)) {
             $this->add_hook('render_page', array($this, 'render_page'));
             $this->register_action('plugin.watermark', array($this, 'watermark_page'));
         }
@@ -59,7 +64,18 @@ class customizr extends rcube_plugin
             $this->rcmail->output->set_env('blankpage', $url);
         }
 
-        // TODO: replace URL for <link rel="shortcut icon" href="xxx" />
+        // replace URL for <link rel="shortcut icon" href="xxx" />
+        if (!is_null($this->custom_favicon)) {
+            $favicon = !empty($this->custom_favicon) ?
+                $favicon = html::tag('link', array('rel' => 'shortcut icon', 'href' => $this->custom_favicon)) :
+                '';
+
+            $args['content'] = preg_replace('!<link\s[^>]*rel="shortcut icon"[^>]*>!', $favicon, $args['content'], -1, $count);
+
+            // append favicon if not found in template
+            if (!$count && $favicon)
+                $args['content'] = preg_replace('!(</head>)!', $favicon . "\n\\1", $args['content']);
+        }
 
         // append custom stylesheet
         if (!empty($this->custom_css)) {
